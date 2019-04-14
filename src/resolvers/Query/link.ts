a5d513da268ba218841e4b1dd5816e550abdcbc9
Gustavo Cabral <andrescabral.c@gmail.com>
2019-04-14 18:27:06 -0400

    feat: add filter to feed

--- src/resolvers/Query/link.ts
+++ src/resolvers/Query/link.ts
@@ -1,7 +1,16 @@
 import { Context } from '../../utils';
 
 export default {
-  feed(parent: any, args: any, context: Context) {
-    return context.prisma.links();
+  async feed(parent: any, args: any, context: Context) {
+    const where = args.filter
+      ? {
+          OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter }
+          ]
+        }
+      : {};
+
+    return context.prisma.links({ where });
   }
 };
